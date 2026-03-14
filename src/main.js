828238aa68654f3cde6cbd6aaeed9854a538d669
eldendo <marc.dendooven@gmail.com>
2026-03-14 11:57:21 +0100

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import React, {StrictMode} from 'https://esm.sh/react@19.0.0';
 import {createRoot} from 'https://esm.sh/react-dom@19.0.0/client';
 import App from './App.js';
-import './index.css';
 
 createRoot(document.getElementById('root')).render(
   React.createElement(StrictMode, null,
